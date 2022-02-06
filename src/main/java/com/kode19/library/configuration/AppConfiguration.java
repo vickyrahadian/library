45bb2c3303cf5a0921584ca5053b1daae6af6d39
vicky <vicky.rahadian@gmail.com>
2022-02-06 22:12:22 +0700

    add properties for pointing external data source

--- src/main/java/com/kode19/library/configuration/AppConfiguration.java
+++ src/main/java/com/kode19/library/configuration/AppConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("file:d:/config.properties")
+@PropertySource("file:/home/rhel/config/config.properties")
 public class AppConfiguration {
 
 
